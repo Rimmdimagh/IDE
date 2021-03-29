1a89d179b5d6d686d70afdcc8c076d27f530f381
rimmdimagh <rim.mdimagh@esprit.tn>
2021-03-29 03:27:26 -0700

    mise a jours

---
+++ personnage.h
@@ -0,0 +1,31 @@
+
+#ifndef INPUT_H_INCLUDED
+#define INPUT_H_INCLUDED
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+
+typedef struct 
+{
+SDL_Surface *sprite;
+SDL_Rect posperso;
+int vie,score;
+SDL_Rect scorePos;
+SDL_Rect posvie;
+
+int direction;
+double vitesse;
+double acceleration;
+}perso;
+
+
+void initPerso(Personne *p);
+void afficherPerso(Personne p,SDL_Surface *screen);
+void deplacerPerso(Personne *p,Uint32 dt);
+void free_perso(perso *p);
+
+
+#endif 
